cb449f1fc653a12047b23640f4db504c02b4c0ef
fangqingbiao <fangqingbiao@unisound.com>
2020-07-09 01:36:13 +0800

    :construction:

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,38 +1,46 @@
 package sort;
 
+import java.util.Arrays;
+
 /**
- * 快速排序：运用了分治思想，取某个支点值，将数组依次从左-->右进行遍历（寻找直到大于支点的数）和从右-->左进行遍历（寻找直到小于支点的数），
- * 			然后交换位置，使得左边的元素小于支点数值，右边元素大于支点数值
+ * 快速排序：运用了分治思想，取某个基准点值，将数组依次从左-->右进行遍历（寻找直到大于基准点的数）和从右-->左进行遍历（寻找直到小于基准点的数），
+ * 然后交换位置，使得左边的元素小于基准点数值，右边元素大于基准点数值
  */
 public class QuickSort {
-    static int k = 0;
+	static int k = 0;
 
-    public static void sort(int[] nums, int L, int R) {
-        int i = L;
-        int j = R;
-        // 支点
-        int m = (L + R) / 2;
-        int p = nums[m];
-        while (i <= j) {
-            //从左-->右扫描，寻找直到比支点大于的数
-            while (p > nums[i]) i++;
+	public static int selectMid(int low, int mid, int high) {
+		return low <= mid ? (mid <= high ? mid : Math.max(low, high)) : (low <= high ? low : Math.max(mid, high));
+	}
 
-            //从右-->左扫描，寻找直到比支点小于的数
-            while (p < nums[j]) j--;
-            //此时已经分别找到了比支点小的数(右边)、比支点大的数(左边)，它们进行交换
-            if (i <= j) {
-                int t = nums[i];
-                nums[i] = nums[j];
-                nums[j] = t;
-                i++;
-                j--;
-                k++;
-            }
-        }
-        //上面一个while保证了第一趟排序支点的左边比支点小，支点的右边比支点大。
-        //“左边”再做排序，直到左边剩下一个数(递归出口)
-        if (L < j) sort(nums, L, j);
-        //“右边”再做排序，直到右边剩下一个数(递归出口)
-        if (i < R) sort(nums, i, R);
-    }
+	public static void sort(int[] nums, int L, int R, boolean flag) {
+		int i = L;
+		int j = R;
+		// 基准点
+		int m = (L + R) / 2;
+		int p;
+		if (flag) p = nums[m];
+		else p = selectMid(nums[L], nums[m], nums[R]);
+		while (i <= j) {
+			//从左-->右扫描，寻找直到比基准点大于的数
+			while (p > nums[i]) i++;
+
+			//从右-->左扫描，寻找直到比基准点小于的数
+			while (p < nums[j]) j--;
+			//此时已经分别找到了比基准点小的数(右边)、比基准点大的数(左边)，它们进行交换
+			if (i <= j) {
+				int t = nums[i];
+				nums[i] = nums[j];
+				nums[j] = t;
+				i++;
+				j--;
+				k++;
+			}
+		}
+		//上面一个while保证了第一趟排序基准点的左边比基准点小，基准点的右边比基准点大。
+		//“左边”再做排序，直到左边剩下一个数(递归出口)
+		if (L < j) sort(nums, L, j, flag);
+		//“右边”再做排序，直到右边剩下一个数(递归出口)
+		if (i < R) sort(nums, i, R, flag);
+	}
 }
--- src/sort/SortTest.java
+++ src/sort/SortTest.java
@@ -6,22 +6,29 @@ import java.util.Random;
 
 public class SortTest {
     public static void main(String[] args) {
-        int[] nums = new int[10];
+        int[] nums = new int[100000];
         Random r = new Random();
         for (int i = 0; i < nums.length; i++) {
-            nums[i] = r.nextInt(100);
+            nums[i] = r.nextInt(100000);
         }
-        nums = new int[]{54, 50, 59, 60, 88, 53, 64, 76, 88, 17};
+//        nums = new int[]{10,9,8,7,6,5,4,3,2,1};
         System.out.println("排序前：" + Arrays.toString(nums));
         long starTime = System.currentTimeMillis();
 //        BubbleSort.sort(nums);
 //        SelectSort.sort(nums);
-        InsertSort.sort(nums);
-//        QuickSort.sort(nums, 0, nums.length-1);
-//        System.out.println(QuickSort.k);
+//        InsertSort.sort(nums);
+		int[] copyNums  = Arrays.copyOf(nums, 100000);
+		//取中点值
+        QuickSort.sort(nums, 0, nums.length-1, true);
+        System.out.println(QuickSort.k);
+//		System.out.println("copyNums:"+Arrays.toString(copyNums));
+		//取中间值
+		QuickSort.sort(copyNums, 0, nums.length-1, false);
+		System.out.println(QuickSort.k);
         long endTime = System.currentTimeMillis();
         System.out.println("排序后：" + Arrays.toString(nums));
         System.out.println("耗时：" + (endTime - starTime));
 
     }
+
 }
