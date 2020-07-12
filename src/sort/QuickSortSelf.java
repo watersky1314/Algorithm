ec5a9646524a531df419b755d06e96c2549f9984
fangqingbiao <fangqingbiao@unisound.com>
2020-07-13 01:37:28 +0800

    :construction:

--- src/sort/QuickSortSelf.java
+++ src/sort/QuickSortSelf.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class QuickSortSelf {
 
 	public static void sort(int[] nums, int leftBound, int rightBound) {
+		if (leftBound >= rightBound) return;
 		//选取基准值
 		int pivot = nums[leftBound];
 		//选取前后两指针
@@ -21,6 +22,8 @@ public class QuickSortSelf {
 		System.out.println("left=" + left + ",right=" + right);
 		System.out.println(Arrays.toString(nums));
 		swap(nums, leftBound, right);
+		sort(nums, leftBound, right - 1);
+		sort(nums, right + 1, rightBound);
 	}
 
 	public static void swap(int[] nums, int left, int right) {
@@ -34,7 +37,7 @@ public class QuickSortSelf {
 		for (int i = 0; i < nums.length; i++) {
 			nums[i] = (int) (Math.random() * 100);
 		}
-		nums = new int[]{5,5, 76, 14, 57, 4, 61, 29, 72, 2, 18, 5};
+		nums = new int[]{2, 5, 76, 14, 57, 4, 61, 29, 72, 2, 18, 5};
 		System.out.println("排序前：" + Arrays.toString(nums));
 		sort(nums, 0, nums.length - 1);
 		System.out.println("排序后：" + Arrays.toString(nums));
