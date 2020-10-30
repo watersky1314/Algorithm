844077318507e3850c697dbda0a396584a654067
fangqingbiao <fangqingbiao@unisound.com>
2020-10-30 19:09:16 +0800

    :constructor: 快排

---
+++ src/sort/TestDemo.java
@@ -0,0 +1,17 @@
+package sort;
+
+import java.util.*;
+
+public class TestDemo {
+	public static void main(String[] args) {
+//		OptionalInt value = null;
+//		int sort = Optional.of(value).get().orElse(0);
+//		System.out.println(sort);
+
+//		List<Long> ids = new ArrayList<>();
+//		ids.add(1L);
+//		ids.add(2L);
+//		ids.add(3L);
+//		System.out.println(JSO);
+	}
+}
