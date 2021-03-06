7409cb5b3f3b5eb07e75d6e601cbf4d4bd9bf408
fangqingbiao <fangqingbiao@unisound.com>
2021-03-06 18:32:15 +0800

    :constructor: 基数排序

---
+++ src/search/BinarySearch.java
@@ -0,0 +1,28 @@
+package search;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+		int[] nums = {1, 3, 3, 3, 3, 6, 7, 8, 9, 10};
+		int n = binarySearch(nums, 3);
+		System.out.println(n);
+
+	}
+
+	static int binarySearch(int[] nums, int target) {
+		int left = 0;
+		int right = nums.length - 1; // 注意
+
+		while (left <= right) { // 注意
+			int mid = left + (right - left) / 2;
+			System.out.println("left=" + left + ", right=" + right);
+			if (nums[mid] == target)
+				return mid;
+			else if (nums[mid] < target)
+				left = mid + 1; // 注意
+			else
+				right = mid - 1; // 注意
+		}
+		return -1;
+	}
+}
--- src/sort/BucketSort.java
+++ src/sort/BucketSort.java
@@ -1,7 +1,28 @@
 package sort;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 桶排序：线性时间、非比较的排序算法，将数组
  */
 public class BucketSort {
+
+		public static int majorityElement(int[] nums) {
+			int m = nums.length>>1;
+			Map<Integer,Integer> countMap = new HashMap<>();
+			for (int j : nums) {
+				countMap.putIfAbsent(j, 0);
+				if(countMap.get(j) + 1 > m) return j;
+				countMap.put(j, countMap.get(j) + 1);
+
+			}
+			return 0;
+
+		}
+
+	public static void main(String[] args) {
+		int[] nums = {3,2,3};
+		System.out.println(majorityElement(nums));
+	}
 }
